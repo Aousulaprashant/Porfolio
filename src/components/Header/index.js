c1906ecacfa2a94d8e16cd2dfa39499bfcc2f425
PRASHANTH AOUSULA <prashanthchary91595@gmail.com>
2025-06-23 21:25:47 +0530

    fixed navBar animation and some responsive design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -39,7 +39,7 @@ const Header = () => {
           ))}
         </nav>
 
-        {/* Mobile Hamburger */}
+        {/* Mobile Menu Button */}
         <div className="md:hidden z-50">
           <button onClick={() => setMenuOpen(!menuOpen)}>
             {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
@@ -47,21 +47,29 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="md:hidden bg-slate-50 shadow-inner px-5 pb-4 pt-2 space-y-3 font-semibold text-gray-700">
+      {/* Mobile Menu: Always Rendered, Smooth Transition */}
+      <div
+        className={`md:hidden overflow-hidden bg-slate-50 shadow-inner px-5 transition-all duration-500 ease-in-out ${
+          menuOpen ? "max-h-96 pt-2 pb-4" : "max-h-0"
+        }`}
+      >
+        <div
+          className={`transition-opacity duration-300 ${
+            menuOpen ? "opacity-100" : "opacity-0"
+          }`}
+        >
           {NavItems.map((item) => (
             <a
               key={item.name}
               href={item.link}
               onClick={() => setMenuOpen(false)}
-              className="block hover:text-[#367cbdf9] transition-all"
+              className="block py-1 font-semibold text-gray-700 hover:text-[#367cbdf9] transition-all"
             >
               {item.name}
             </a>
           ))}
         </div>
-      )}
+      </div>
     </header>
   );
 };
